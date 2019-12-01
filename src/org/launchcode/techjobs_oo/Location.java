489827d0848a4994e4a90893825bdb68f4070fce
Eric <eric.earlywine@hotmail.com>
2019-12-01 13:45:18 -0600

    First few classes finished

--- src/org/launchcode/techjobs_oo/Location.java
+++ src/org/launchcode/techjobs_oo/Location.java
@@ -15,7 +15,10 @@ public class Location {
     // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
     //  constructor should also call the empty constructor in order to initialize the 'id' field.
 
-
+    public Location(String locale) {
+        this();
+        this.value = locale;
+    }
     // Custom toString, equals, and hashCode methods:
 
     @Override
